4d5fb1b37448e74c8cca9eff49ee7be3963a38a8
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2022-01-13 23:17:53 +0100

    Add domains

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -175,3 +175,30 @@ resource "cloudflare_record" "prowlarr" {
   type    = "CNAME"
   ttl     = 1
 }
+
+resource "cloudflare_record" "alert" {
+  name    = "alert"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  proxied = false
+  type    = "CNAME"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "prometheus" {
+  name    = "prometheus"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  proxied = false
+  type    = "CNAME"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "grafana" {
+  name    = "grafana"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  proxied = false
+  type    = "CNAME"
+  ttl     = 1
+}
