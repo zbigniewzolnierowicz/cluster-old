00348a9dc5ebcd80f665b8e97137f3515077335e
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2022-04-17 21:14:06 +0200

    Add Tdarr domain to Terraform

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -220,3 +220,12 @@ resource "cloudflare_record" "lidarr" {
   type    = "CNAME"
   ttl     = 1
 }
+
+resource "cloudflare_record" "tdarr" {
+  name    = "tdarr"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  proxied = false
+  type    = "CNAME"
+  ttl     = 1
+}
