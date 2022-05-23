5e3e846700df167a9b51a561d7a1058f7a0f8575
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2022-05-23 22:43:42 +0200

    page configs

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -94,7 +94,7 @@ resource "cloudflare_record" "ipv4" {
 resource "cloudflare_record" "root" {
   name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  value   = "zbgnwpage.netlify.app."
   proxied = true
   type    = "CNAME"
   ttl     = 1
@@ -234,6 +234,15 @@ resource "cloudflare_record" "authelia" {
   name    = "auth"
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
   value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  proxied = false
+  type    = "CNAME"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "netlify_homepage_www" {
+  name    = "www"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "zbgnwpage.netlify.app."
   proxied = false
   type    = "CNAME"
   ttl     = 1
