a6112c582a356c37132f89a1488c3bcf233c7c62
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2022-07-10 01:13:09 +0200

    Remove root

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -91,11 +91,11 @@ resource "cloudflare_record" "ipv4" {
   ttl     = 1
 }
 
-resource "cloudflare_record" "root" {
-  name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
-  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
-  proxied = true
-  type    = "CNAME"
-  ttl     = 1
-}
+# resource "cloudflare_record" "root" {
+#   name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
+#   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+#   value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+#   proxied = true
+#   type    = "CNAME"
+#   ttl     = 1
+# }
