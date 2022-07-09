9dc54624dbaa1c7221e1f7396b2780f8bee9bcf3
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2022-07-10 01:15:25 +0200

    Bring back me page

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -91,11 +91,20 @@ resource "cloudflare_record" "ipv4" {
   ttl     = 1
 }
 
-# resource "cloudflare_record" "root" {
-#   name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
-#   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-#   value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
-#   proxied = true
-#   type    = "CNAME"
-#   ttl     = 1
-# }
+resource "cloudflare_record" "root" {
+  name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "76.76.21.21"
+  proxied = false
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "www" {
+  name    = "www"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "cname.vercel-dns.com"
+  proxied = false
+  type    = "CNAME"
+  ttl     = 1
+}
